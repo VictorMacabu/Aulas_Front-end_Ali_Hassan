27342cda925f564490f50314ecc8ac7a567ab6cb
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-06-13 23:46:27 -0300

    inserindo js

---
+++ jogo_da_velha/js_jdv.js
@@ -0,0 +1,16 @@
+const jogador1 = "X";
+const jogador2 = "O";
+
+var jogadorDaVez = jogador1;
+var finalJogo = false;
+
+atualizar();
+
+function atualizar(){
+    if(finalJogo){
+        return;
+    }
+    if(jogadorDaVez == jogador1){
+        let jogador = document.querySelector([])
+    }
+}

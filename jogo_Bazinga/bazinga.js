c7c636c52ceb1ad0837594e1aaba8983590e3eb2
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-06-28 21:04:53 -0300

    update project

--- jogo_Bazinga/bazinga.js
+++ jogo_Bazinga/bazinga.js
@@ -2,11 +2,31 @@
 
 var ptsRaj = 0;
 var ptsSheldon = 0;
-const qtdJogadas = document.getElementById('qtdJogadas').value;
+var placarAtual = `${ptsRaj} X ${ptsSheldon}`;
 
+var qtdJogadas = 0;
+var jogadas = document.getElementsByClassName('imgJogada');
 
-for(var index= 0; index < 10; index++){
-    ptsRaj++
-    const placar = `${ptsRaj} X ${ptsSheldon}`;
-    document.getElementById('placar').innerHTML = placar;
+let jogadasFeitas= [];
+
+var placar = document.getElementById('placar').innerHTML = placarAtual;
+
+function rodadas() {
+      let selecionar = document.getElementById("btnSelecionar");
+      selecionar.addEventListener("click",qtdJogadas = document.getElementById("qtdJogadas").value);
+        if(qtdJogadas > 0){
+            let jogadaAtual = document.getElementsByClassName("imgJogada").addEventListener("click",getAttribute("jogada"));
+            let exibirJogada = `[ ${this.jogadaAtual} ]`;
+            for(let i = 0;i < qtdJogadas; i++){
+                
+                document.getElementById("jogadasFeitas").innerHTML = exibirJogada;
+                this.exibirJogada += this.exibirJogada;
+            }
+        }
+}
+
+function reiniciar(){
+    var reload = document.getElementsByClassName("reiniciarJogo");
+    reload[0].addEventListener("click",window.location.reload());
+    console.log("reload")
 }

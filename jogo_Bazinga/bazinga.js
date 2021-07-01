d96cf688d491c629a72c96f5f088c27220538a12
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-06-30 22:35:29 -0300

    update qtdJogadas

--- jogo_Bazinga/bazinga.js
+++ jogo_Bazinga/bazinga.js
@@ -8,44 +8,44 @@ let qtdJogadas = 0;
 let jogadas = document.getElementById('jogadas');
 
 let selecionar = document.getElementById("btnSelecionar");
-let exibirJogada = document.getElementById("jogadasFeitas").innerHTML;
+let exibirJogada = document.getElementById("jogadasFeitas");
 
-let jogadasFeitas= [];
-var jogadaAtual ='';
+let jogadasFeitas = [];
+var jogadaAtual = '';
 
 placar();
 
 
-function placar(){ // atualiza o mostrador do placar 
+function placar() { // atualiza o mostrador do placar 
     document.getElementById('placar').innerHTML = placarAtual;
 }
 
-function numJogadas(){// recebe a quantidade de jogadas selecionadas e põe na tela
+function numJogadas() {// recebe a quantidade de jogadas selecionadas e põe na tela
     qtdJogadas = document.getElementById("qtdJogadas").value
-    console.log( qtdJogadas);
-        if(qtdJogadas > 0){
-            jogadas.addEventListener('click',function(j){
-              jogadaAtual = j.target.this.jogada;
-              exibirJogada.innerHTML = `[ ${jogadaAtual} ] `;
-            });
-           
-            
-            for(let i = 0;i < qtdJogadas; i++){
-                this.exibirJogada += this.exibirJogada;
-            }
+    console.log(qtdJogadas); //teste
+
+    if (qtdJogadas > 0) {
+        jogadas.addEventListener('click', function (j) {
+            jogadaAtual = j.getAttribute("jogada");
+        });
+        exibirJogada.innerHTML = ` Jogada(s) [ ${jogadaAtual} ]`;
+
+        for (let i = 0; i < qtdJogadas; i++) {
+            this.exibirJogada += this.exibirJogada;
         }
+    }
 }
 selecionar.addEventListener("click", numJogadas);
 
- function rodadas() { // verifica as jogadas e adiciona na tela
-       //seleciona a quantidade de jogadas
-      
-     
+function rodadas() { // verifica as jogadas e adiciona na tela
+    //seleciona a quantidade de jogadas
+
+
 }
 
 
 
-    document.getElementsByClassName("reiniciarJogo")[0].onclick = function reload(){
+document.getElementsByClassName("reiniciarJogo")[0].onclick = function reload() {
     window.location.reload();
     console.log("reload")
 }

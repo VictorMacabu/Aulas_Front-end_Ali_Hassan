6c2312baad32d98f9f8bee8abf3fdbbf406ba749
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-07-01 23:41:31 -0300

    update seletor

--- jogo_Bazinga/bazinga.js
+++ jogo_Bazinga/bazinga.js
@@ -5,13 +5,19 @@ let ptsSheldon = 0;
 let placarAtual = `${ptsRaj} X ${ptsSheldon}`;
 
 let qtdJogadas = 0;
-let jogadas = document.getElementById('jogadas');
+let jogadas = document.querySelectorAll(".imgJogada");
 
 let selecionar = document.getElementById("btnSelecionar");
 let exibirJogada = document.getElementById("jogadasFeitas");
 
 let jogadasFeitas = [];
-var jogadaAtual = '';
+let jogadaAtual = '';
+
+const pedra = document.getElementById("pedra1").getAttribute("jogada");     // jogadas
+const papel = document.getElementById("papel1").getAttribute("jogada");     // jogadas
+const tesoura = document.getElementById("tesoura1").getAttribute("jogada"); // jogadas
+const lagarta = document.getElementById("lagarta1").getAttribute("jogada"); // jogadas
+const Spock = document.getElementById("Spock1").getAttribute("jogada");     // jogadas
 
 placar();
 
@@ -20,18 +26,58 @@ function placar() { // atualiza o mostrador do placar
     document.getElementById('placar').innerHTML = placarAtual;
 }
 
+function selectJogada() {
+    if((qtdJogadas > 0)&&( pedra.onclick )){
+        jogadas[0].addEventListener('click', function () {
+        jogadaAtual = jogadas[0].getAttribute("jogada");
+        exibirJogada.innerHTML = ` [ ${jogadaAtual} ]`;
+        qtdJogadas--;
+    })
+};
+if((qtdJogadas > 0)&&( papel.onclick )){
+    jogadas[1].addEventListener('click', function () {
+        jogadaAtual = jogadas[1].getAttribute("jogada");
+        exibirJogada.innerHTML += ` [ ${jogadaAtual} ]`;
+        qtdJogadas--;
+    });
+}
+    if((qtdJogadas > 0)&&( tesoura.onclick )){
+    jogadas[2].addEventListener('click', function () {
+        jogadaAtual = jogadas[2].getAttribute("jogada");
+        exibirJogada.innerHTML += ` [ ${jogadaAtual} ]`;
+        qtdJogadas--;
+    });
+}
+    if((qtdJogadas > 0)&&( lagarta.onclick )){
+    jogadas[3].addEventListener('click', function () {
+        jogadaAtual = jogadas[3].getAttribute("jogada");
+        exibirJogada.innerHTML += ` [ ${jogadaAtual} ]`;
+        qtdJogadas--;
+    });
+}
+    if((qtdJogadas > 0)&&( Spock.onclick )){
+    jogadas[4].addEventListener('click', function () {
+        jogadaAtual = jogadas[4].getAttribute("jogada");
+        exibirJogada.innerHTML += ` [ ${jogadaAtual} ]`;
+        qtdJogadas--;
+    });
+}
+}
+function regraJogadas(qtdJogadas){ //validar nas regras
+    if(qtdJogadas <= 0 ){
+        alert("Número de jogadas inválido")
+    }
+}
+
 function numJogadas() {// recebe a quantidade de jogadas selecionadas e põe na tela
     qtdJogadas = document.getElementById("qtdJogadas").value
     console.log(qtdJogadas); //teste
 
     if (qtdJogadas > 0) {
-        jogadas.addEventListener('click', function (j) {
-            jogadaAtual = j.getAttribute("jogada");
-        });
-        exibirJogada.innerHTML = ` Jogada(s) [ ${jogadaAtual} ]`;
-
-        for (let i = 0; i < qtdJogadas; i++) {
-            this.exibirJogada += this.exibirJogada;
+        exibirJogada.innerHTML = ' Jogadas: ';
+        while (qtdJogadas > 0) {
+            selectJogada()
+            qtdJogadas--;
         }
     }
 }
